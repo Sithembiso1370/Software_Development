8093a242fd535b7c8fd7b0b8533291cee7a8c230
sithembiso1370 <sithembiso.maphanga@gmail.com>
2022-03-15 10:17:30 +0200

    weekly repo update

---
+++ JS/REACT/reactweb/ReactDocsTuts/first-reactdocs-app/src/formatUserName.js
@@ -0,0 +1,13 @@
+
+// In the example below, we embed the result of calling a JavaScript function, formatName(user), 
+// into an <h1> element.
+
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+  
+
+
+
+  export default formatName;
