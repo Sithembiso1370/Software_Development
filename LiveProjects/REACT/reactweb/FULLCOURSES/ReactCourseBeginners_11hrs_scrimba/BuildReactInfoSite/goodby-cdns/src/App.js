25c670989052b60dd50b64fc7a63e2539de6ff39
Sithembiso1370 <maphangasthembiso@gmail.com>
2022-04-28 09:47:43 +0200

    Full courses React for the past 2 weeks

--- LiveProjects/REACT/reactweb/FULLCOURSES/ReactCourseBeginners_11hrs_scrimba/BuildReactInfoSite/goodby-cdns/src/App.js
+++ LiveProjects/REACT/reactweb/FULLCOURSES/ReactCourseBeginners_11hrs_scrimba/BuildReactInfoSite/goodby-cdns/src/App.js
@@ -1,17 +1,38 @@
-import React from "react";
-import PageContent from './Screens/PageContent';
-import './styles/navbar.css'
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import Main from './Main';
 
-
-const App = () => {
-
-  
+function App() {
   return (
     <PageContent/>
   );
 }
 
-
+function PageContent() {
+  return (
+    <div>
+    <Navbar/>
+    <Main/>
+    </div>
+  );
+}
 
 export default App;
 
+     // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
