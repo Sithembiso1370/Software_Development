1deebe98a51c9b3e4625b28d61deb345b3028bb8
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-05-11 11:28:59 +0200

    Started amazon clone DEV COMMUNITY

--- LiveProjects/REACT/reactweb/BUILDINGCLONES/FullStackAMAZONCloneFullECommStorein8Hrs2021/amazon-clone/src/App.js
+++ LiveProjects/REACT/reactweb/BUILDINGCLONES/FullStackAMAZONCloneFullECommStorein8Hrs2021/amazon-clone/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+// Once you’ve done importing the code, you can now use React Router in your file
+import { Routes ,Route } from 'react-router-dom';
+import About from './About';
 import './App.css';
+import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<div><Header /><Home /></div>} />
+        <Route path="about" element={<About />} />
+      </Routes>
+      {/* <a href="/">Home</a>
+      // <a href="about">About</a> */}
+  </div>
   );
 }
 
