12587d33158c0859378efa5fd9abd2a8b390c829
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-05-12 15:18:35 +0200

    Restarted amazon clone youtube

---
+++ LiveProjects/REACT/reactweb/BUILDINGCLONES/FullStackAMAZONCloneFullECommStorein8Hrs2021/yt/full-stack-amazon-clone/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css"
+import Header from './Header'
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className='app'>
+      <h1>Hello Clever Programmer,Lets build the amazon store 🚀</h1>
+      <Header/>
+      {/* HOME COMPONENT */}
+    </div>
+  )
+}
+
+export default App
---
+++ LiveProjects/REACT/reactweb/BUILDINGCLONES/FullStackAMAZONCloneFullECommStorein8Hrs2021/yt/full-stack-amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDk4LO5vXZVey9Ob0QTtjix3JZP3tW5BwY",
+  authDomain: "challenge-5f077.firebaseapp.com",
+  projectId: "challenge-5f077",
+  storageBucket: "challenge-5f077.appspot.com",
+  messagingSenderId: "447568839227",
+  appId: "1:447568839227:web:22ecb7114ee6be3fb3921e",
+  measurementId: "G-K5PG7T5NKS"
+};
