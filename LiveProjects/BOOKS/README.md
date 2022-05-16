8099e4c81ddaeab6afa00d5b97c1b33bceaeebbd
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-05-16 22:01:40 +0200

    Modified My File Structure

---
+++ LiveProjects/BOOKS/README.md
@@ -0,0 +1,3 @@
+# AndroidDev
+Current Android Projects completed once
+A Collection of all android related Projects bussy with and Completed
---
+++ LiveProjects/Cloned/PHP/php-mvc-framework/migrations/m0002_something.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * User: Sithembiso Maphanga
+ * Date: 7/10/2020
+ * Time: 8:07 AM
+ */
+
+class m0002_add_password_column {
+
+    public function up(){
+        echo "Applying migration";
+    }
+
+    // Do the reverse of what I do in the Up
+    public function down(){
+        echo "Down migration";
+    }
+    
+}
